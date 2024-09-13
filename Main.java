2e2cbb6efd00f1337c64bed8d697ed0535aa6c07
EDUARDO CORREIA <97844175+Eduardo1985S@users.noreply.github.com>
2024-09-13 13:02:21 -0300

    mensagem de commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        // Integer variable
+        int myInt = 10;
+        System.out.println("Integer: " + myInt);
+
+        // Floating point variable
+        double myDouble = 5.99;
+        System.out.println("Double: " + myDouble);
+
+        // Character variable
+        char myChar = 'A';
+        System.out.println("Character: " + myChar);
+
+        // Boolean variable
+        boolean myBool = true;
+        System.out.println("Boolean: " + myBool);
+
+        // String variable
+        String myString = "Hello, World!";
+        System.out.println("String: " + myString);
+    }
+}
